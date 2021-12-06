f407fa31a3eff8fde8122eb5a272c73464f81d02
Kolegata99 <80822365+Kolegata99@users.noreply.github.com>
2021-12-06 19:23:02 +0200

    test

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ mongoose(app)
 app.use(cookieParser())
 
 app.use(mainMiddleware())
-
+//sadasdasadasdasdadasasd
 app.use(routes)
 
 app.listen(5001, () => console.log(`Server is running on port 5001...`)) //set up the port and information log
